e990db77cefe723796d63afbdd7f04195fb24291
a-bruno <brunogs1995@gmail.com>
2016-10-17 22:57:30 -0300

    de novo

--- js/test.js
+++ js/test.js
@@ -1,106 +1,65 @@
 var Litamana = Litamana || {},
-	COLLECTION_AUTHORS = [];
+	_COLLECTION = [];
 
-function factoryOfAuthor(id, name, birth, nat, occupation, genre, construction, img1, img2) {
-	this.id = id;
-	this.name = name;
-	this.birth = birth;
-	this.nat = nat;
-	this.occupation = occupation;
-	this.genre = genre;
-	this.construction = construction;
-	this.img1 = img1;
-	this.img2 = img2;
-}
+Litamana.EntityMaminipulation = {
 
-Litamana.GetDataJSONS = {
-	requestJSON: function(argUrlJSON, argObject, argElemWrapr) {
-		if(!argUrlJSON) {
-			console.log('Error: JSON_URL undefined!');
+	getJSON: function(strUrl, strKeyParent, strKeyChild) {
+
+		if(!strUrl) {
+			return false;
 		} else {
 			jQuery.ajax({
-				method: 'POST',
-				url: argUrlJSON,
-				dataType: 'JSON',
+				url: strUrl,
 				async: false,
+				dataType: 'JSON',
+				method: 'POST',
 				statusCode: {
-					404: function() {
-						console.log('file not found!');
+					404: function(){
+						alert('File not found!');
 					}
 				}
-			}).done(function(data){
-				if(argObject) {
-					jQuery.each(data[argObject], function(key, value){
-						COLLECTION_AUTHORS.push(new factoryOfAuthor(value['id'], key, value['nacimento'], value['nacionalidade'], value['ocupacao'], value['genero'], value['obras relevantes'], value['url imagem'], value['url imagem 1']));
-					});
-				}
-			});	
+			})
+			.done(function(data) {
+				jQuery.each(data[strKeyParent], function(key, value){
+					if(!strKeyChild) {
+						jQuery('.map-lista-escritores')
+							.append(
+								'<li id="'+ value['id'] 
+								+'" class="map-lista-author"> <img src="'+ value['url imagem 1'] 
+								+'" alt="Image of author '+ key +'" /></li>'
+							);
+					} else if(strKeyChild == key) {
+						alert(true);
+					}
+
+				});
+			});
 		}
+
 	},
+
 };
 
-Litamana.Authors = function() {
+Litamana.Views = function() {
 	this.init.apply(this, arguments);
-};
+}
 
-Litamana.Authors.prototype = {
+Litamana.Views.prototype = {
 	init: function() {
-		this.JSON_URL = 'js/concepts.json';
-		this.CONTAINER_MAIN = jQuery('.map-container');
-		this.MAP_LIST = jQuery('.map-lista-escritores');
-		this.avatarAuthors();
+		this.BASE_URL = 'js/concepts.json',
+		this.POINTER_PARENT = 'escritores';
 	},
-	avatarAuthors: function() {
-		this.requestJSON(this.JSON_URL, 'escritores');	
-		for(var i in COLLECTION_AUTHORS)
-			jQuery(this.MAP_LIST).append('<li id="'+ COLLECTION_AUTHORS[i].id +'" class="map-lista-author"> <img src="'+ COLLECTION_AUTHORS[i].img2 +'" alt="Image of author '+ COLLECTION_AUTHORS[i].name +'" /></li>');
-	}
-};
 
-jQuery.extend(Litamana.Authors.prototype, Litamana.GetDataJSONS);
-
-Litamana.Authors.DragDrop = function(){
-	this.init.apply(this, arguments);
-};
-
-Litamana.Authors.DragDrop.prototype = {
-	init: function() {
-		this.AUTHOR_AVATAR = jQuery('.map-lista-author');
-		this.drag();
-		this.observeClick();
-		this.observeClickExit();
+	getAllHtmlWriters: function() {
+		this.getJSON(this.BASE_URL, this.POINTER_PARENT);
+		jQuery('.map-lista-author').draggable({ containment: '.map-container', scroll: false });
 	},
-	drag: function() {
-		this.AUTHOR_AVATAR.draggable({ containment: '.map-container', scroll: false });
-	},
-	observeClick: function() {
-		jQuery(this.AUTHOR_AVATAR).click(function() {
-			var HTML_POPUP =
-			'<h1>'+COLLECTION_AUTHORS[jQuery(this).attr('id')].name+'</h1>'+
-	        '<img src="'+COLLECTION_AUTHORS[jQuery(this).attr('id')].img1+'" alt="" />'+
-	        '<div class"wrap-infos-left">'+
-	        '<div class="box"><strong>Nascimento</strong><span> '+COLLECTION_AUTHORS[jQuery(this).attr('id')].birth+'</span></div>'+
-	        '<div class="box"><strong>Nacionalidade</strong> <span> '+COLLECTION_AUTHORS[jQuery(this).attr('id')].nat+'</span></div>'+
-	        '<div class="box"><strong>Ocupação</strong><span> '+COLLECTION_AUTHORS[jQuery(this).attr('id')].nat+'</span></div>'+
-	        '<div class="box"><strong>Gênero</strong><span> '+COLLECTION_AUTHORS[jQuery(this).attr('id')].genre+'</span></div>'+
-	        '<div class="box"><strong>Obras</strong><span> '+COLLECTION_AUTHORS[jQuery(this).attr('id')].construction+'</span></div>';
+}
 
-	        jQuery('.map-popup--escritores .wrap').html('');
-			jQuery('.map-popup--escritores .wrap').append(HTML_POPUP);
-			jQuery('.map-popup--escritores').show();
-		});
-	},
-	observeClickExit: function() {
-		jQuery('.btn-fechar').click(function() {
-			console.log(this);
-			jQuery(this).parent().hide();
-		});
-	}
-};
+jQuery.extend(Litamana.Views.prototype, Litamana.EntityMaminipulation);
 
-var author = new Litamana.Authors();
-var dragdrop = new Litamana.Authors.DragDrop();
+jQuery(document).ready(function() {
+	new Litamana.Views().getAllHtmlWriters();
 
-
-
-
+	
+});
