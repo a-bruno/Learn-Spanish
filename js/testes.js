76058f8ff9f10418086da95dfc88ad88764a359b
a-bruno <brunogs1995@gmail.com>
2016-11-08 00:20:21 -0300

    agora vai

--- js/testes.js
+++ js/testes.js
@@ -1,4 +1,5 @@
 var Litaman = Litaman || {};
+var $ = jQuery;
 
 Litaman.Beans = {
 	bean: {},
@@ -22,21 +23,59 @@ Litaman.ManagerBeans.prototype = {
 		this.TYPE_FILE = 'JSON';
 		this.nameToBean = 'jalo';
 
-		this.jSON('dados.json', this);
+		this.jSON('dados.json', this, this.KEY_JSON_MAIN);
 	},
-	jSON: function(nameFile, that) {
+	jSON: function(nameFile, that, keyMain) {
 		$.ajax({
 			url: that.PATH_URL + nameFile,
 			dataType: that.typeFile,
 			async: false
 		})
-		.done(function(data){
-			 $.each(data, function(key, value) {
-				that.germination(that.nameToBean, data[key]);
-			 });
+		.done(function(data){   
+             $.each(data[''+ keyMain +''], function(key, value) {
+                that.germination(that.nameToBean, value);
+             });
 		});
 	},
-	html: function() {
+	html: function(qualHTML, elem) {
+        switch(qualHTML) {
+            case 'list-avatar':
+                    for(var i in this.bean['jalo']) {    
+                        $('.map-lista-escritores').append(
+                            '<li id="'+ this.bean['jalo'][i]['id'] +'" class="map-lista-author" locked="true"> <img src="'+ this.bean['jalo'][i]['url imagem 1'] + '" /></li>'
+                        );
+                    }
+                    $('.map-lista-escritores').find('li').draggable({ containment: '.map-container', scroll: false }); 
+                break;
+            case 'popup-informacoes': 
+                    $('.full-shadow').addClass('is-active-shadow');
+                    $('.map-popup--escritores').show();
+                
+                    $('.map-popup--escritores').find('h1').text(this.bean['jalo'][elem.attr('id')]['nome']);
+                    $('.wrap img').attr('src',this.bean['jalo'][elem.attr('id')]['url imagem']);
+                
+                    $('.informacoes').html(
+                        '<div class="group"> <strong>Nascimento</strong>' +
+                        '<span>'+ this.bean['jalo'][elem.attr('id')]['nascimento'] +'</span> </div>' +
+                        '<div class="group"> <strong>Nacionalidade</strong>' +
+                        '<span>'+ this.bean['jalo'][elem.attr('id')]['nacionalidade'] +'</span> </div>' +
+                        '<div class="group"> <strong>Ocupação</strong>' +
+                        '<span>'+ this.bean['jalo'][elem.attr('id')]['ocupacao'] +'</span> </div>' +
+                        '<div class="group"> <strong>Gênero</strong>' +
+                        '<span>'+ this.bean['jalo'][elem.attr('id')]['genero'] +'</span> </div>' +
+                        '<div class="group"> <strong>Obras</strong>' +
+                        '<span>'+ this.bean['jalo'][elem.attr('id')]['obras relevantes'] +'</span> </div>'
+                    );
+                
+                    $('.btn-fechar').click(function() {
+                        $('.map-popup--escritores').hide();
+                        $('.full-shadow').removeClass('is-active-shadow');
+                    })
+                break;
+            default:
+                console.log('Not!');
+            break;
+        }
 		
 	}
 };
@@ -45,11 +84,11 @@ $.extend(Litaman.ManagerBeans.prototype, Litaman.Beans);
 
 (function() {
 	var stub = new Litaman.ManagerBeans();
-	var mountHtmlWriters = function() {
-		console.log(this.bean);
-	};
-
-	mountHtmlWriters();
+	
+    stub.html('list-avatar');
+    $('.map-lista-author').click(function() {
+        stub.html('popup-informacoes', $(this));
+    });
 })();
 
 
